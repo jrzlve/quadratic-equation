f6344a5b40587f65e508ade269aebc902b7fc5ec
jrzlve <jrzlve@gmail.com>
2018-09-25 18:19:11 +0300

    v.1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    const result = [];
+    const separator1 = (' * x^2 '), separator2 = (' * x ');
+    let str = equation.replace(separator1, ' ');
+    str = str.replace(separator2, ' ');
+    const array = str.split(' ');
+    const a = +(array[0]), b = +(array[1] + array[2]), c = +(array[3] + array[4]);
+    const D = Math.sqrt(b*b-4*a*c);
+    const x1 = Math.round( (-b + D) / (2 *a) );
+    const x2 = Math.round( (-b - D) / (2 *a) );
+
+    result.push(x1);
+    result.push(x2);
+    result.sort(function(a, b){return a-b});
+
+    return result;
 }
